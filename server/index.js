067d9ca03f2d376b775abd1aae1a890bf3667cc3
karen <tan42204@gmail.com>
2026-07-27 12:32:14 +0800

    fix: add missing path import in server/index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const crypto = require('crypto');
+const path = require('path');
 const astrology = require('./astrology.js');
 
 const app = express();
